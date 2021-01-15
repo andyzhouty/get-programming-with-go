3b35f88ac9ac621c00349a733861891d11da5cfb
andyzhou <andyforever0108@outlook.com>
2021-01-15 16:22:19 +0800

    Add mars program

---
+++ chapter02/mars.go
@@ -0,0 +1,13 @@
+// 我的减重程序
+package main
+
+import "fmt"
+
+// main是所有程序的起始函数
+func main() {
+	fmt.Print("My weight on the surface of Mars is ")
+	fmt.Print(149.0 * 0.3783)
+	fmt.Print(" lbs, and I would be ")
+	fmt.Print(41 * 365 / 687)
+	fmt.Print(" years old")
+}
---
+++ chapter02/mars_my_own.go
@@ -0,0 +1,13 @@
+// 我的减重程序
+package main
+
+import "fmt"
+
+// main是所有程序的起始函数
+func main() {
+	fmt.Print("My weight on the surface of Mars is ")
+	fmt.Print(55 * 0.3783)
+	fmt.Print(" kg, and I would be ")
+	fmt.Print(14 * 365 / 687)
+	fmt.Print(" years old")
+}
