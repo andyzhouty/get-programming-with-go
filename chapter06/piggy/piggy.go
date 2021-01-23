a0be8f075e6673bc77e3e4526c12898fc6bfea7c
andyzhou <andyforever0108@outlook.com>
2021-01-23 18:11:01 +0800

    Finish Chapter 6

---
+++ chapter06/piggy/piggy.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	var piggyBank float64
+	for piggyBank <= 20.0 {
+		deposit_id := rand.Intn(3)
+		var deposit float64
+		switch deposit_id {
+		case 0:
+			deposit = 0.05
+		case 1:
+			deposit = 0.10
+		case 2:
+			deposit = 0.25
+		}
+		piggyBank += deposit
+		fmt.Printf("Deposit %.2f, Total %.2f\n", deposit, piggyBank)
+	}
+}
